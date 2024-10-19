03c8b8606da35310a79cd5a6fe5a663f70ed220f
Joudi Saloukha <159825158+JoudiSaloukha@users.noreply.github.com>
2024-10-19 13:01:09 +0300

    Update calculator.js

--- assests/js/calculator.js
+++ assests/js/calculator.js
@@ -94,7 +94,7 @@
             case '*':
                 return num1 * num2;
             case '/':
-                return num1 / num2;
+                return num2 !== 0 ? num1 / num2 : 'Error';
             default:
                 return num1;
         }
